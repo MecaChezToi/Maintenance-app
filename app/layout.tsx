e2fd1e8dbd489e923fb57dd0dd19951db1deee48
Stephane <schemansteph@gmail.com>
2026-05-27 16:20:23 +0200

    Add PWA icons + fix icon paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,8 @@ export const metadata: Metadata = {
     title: 'FixOps',
   },
   icons: {
-    icon: '/icon-192.png',
-    apple: '/icon-192.png',
+    icon: '/icons/icon-192.png',
+    apple: '/icons/icon-192.png',
   },
 }
 
